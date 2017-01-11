8f394206470faf525be4a96e609fd805d03a49ac
Muhammed Mohsen <muhammedmohsen@kernel.blue>
2017-01-11 17:46:33 +0200

    #2 Calling Youtube to check video availability based on priority

---
+++ urlcheck.php
@@ -0,0 +1,30 @@
+<h1>Check Url </h1>
+
+<?php
+include 'dbconfig.php';
+$p= $_GET["p"];
+$query = mysql_query("SELECT * FROM urls  WHERE priority = ".$p." ORDER BY `urls`.`timestamp` DESC");
+while ($rows = mysql_fetch_array($query)):
+  ifAvailable($rows['id'],$rows['youtube_id'],$apiPublicKey);
+endwhile;
+
+function ifAvailable ($id,$videoID,$apiPublicKey){
+
+$response = file_get_contents('https://www.googleapis.com/youtube/v3/videos?part=id&id='. $videoID . '&key='. $apiPublicKey);
+$json = json_decode($response,true);
+
+if (sizeof($json['items'])) {
+    // video exists
+
+    echo ' <div>'.$videoID.' Video exists</div>';
+    mysql_query("UPDATE urls SET response='available' WHERE id=".$id);
+
+} else {
+    // video does not exist
+    echo ' <div>'.$videoID.' <b> Video does not exist</b><br>';
+    mysql_query("UPDATE urls SET response='unavailable' WHERE id=".$id);
+
+}
+}
+
+?>
